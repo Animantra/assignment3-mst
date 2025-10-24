b45112a9c03edad3076aabf93820687faf2a4242
Animantra <t20227893@gmail.com>
2025-10-25 03:39:48 +0500

    json

---
+++ src/main/java/com/example/readFromJSON.java
@@ -0,0 +1,88 @@
+package com.example;
+
+import com.example.Graphs.Edge;
+import com.example.Graphs.EdgeStr;
+import com.example.Graphs.Graph;
+import com.example.Graphs.GraphCont;
+import com.google.gson.Gson;
+import java.io.FileReader;
+
+import java.util.*;
+
+public class readFromJSON {
+
+    public static class resultData{
+        List<EdgeStr> mstEdges;
+        int totalCost;
+        int operationCount;
+        double exTime;
+
+        public resultData(List<Edge> edges, int cost, int operations,double time){
+            mstEdges = new ArrayList<>();
+
+            for(Edge e : edges){
+                String from = String.valueOf((char)(e.getU() + 'A'));
+                String to = String.valueOf((char)(e.getV() + 'A'));
+                mstEdges.add(new EdgeStr(from, to, e.getW()));
+            }
+            totalCost = cost;
+            operationCount = operations;
+            exTime = time;
+
+        }
+    }
+    public static void main(String[] args) {
+        try (FileReader reader = new FileReader("input.json")){
+            Gson gson = new Gson();
+            GraphCont cont = gson.fromJson(reader, GraphCont.class);
+
+            List<writeToJSON.resultMST> results = new ArrayList<>();
+
+            for(Graph g : cont.getGraphs()){
+
+                List<Edge> edgeInd = new ArrayList<>();
+
+                for(EdgeStr e : g.getEdges()){
+
+                    int u = e.getFrom().charAt(0) - 'A';
+                    int v = e.getTo().charAt(0) - 'A';
+                    int w = e.getWeight();
+
+                    edgeInd.add(new Edge(u, v,w));
+                }
+
+                int n = g.getNodes().size();
+
+                int cost = KruskalAlgo.kruskalMST(n, edgeInd);
+                List<Edge> mst = new KruskalAlgo().getMST();
+                int operations = new KruskalAlgo().getOC();
+                double timeMs = new KruskalAlgo().getTime();
+
+                int cost2 = PrimAlgo.primMst(n, edgeInd);
+                List<Edge> mst2 = new PrimAlgo().getMST();
+                int operations2 = new PrimAlgo().getOC();
+                double timeMs2 = new PrimAlgo().getTime();
+
+
+                resultData kruskalResult = new resultData(mst, cost, operations, timeMs);
+                resultData primResult = new resultData(mst2, cost2, operations2, timeMs2);
+                
+                writeToJSON.resultMST res = new writeToJSON.resultMST(
+                    g.getId(),
+                    n,
+                    g.getEdges().size(),
+                    primResult,
+                    kruskalResult
+                );
+
+                results.add(res);
+            }
+
+            writeToJSON.outputDataToJSON(results);
+            
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+    
+}
---
+++ src/main/java/com/example/writeToJSON.java
@@ -0,0 +1,49 @@
+package com.example;
+
+import java.io.FileWriter;
+import java.util.List;
+import java.util.Map;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+public class writeToJSON {
+    public static class resultMST{
+        private int graph_id;
+        private Map<String,Integer> input_stats;
+        private readFromJSON.resultData prim;
+        private readFromJSON.resultData kruskal;
+
+        public resultMST(int id, int vertices, int edges, readFromJSON.resultData prim, readFromJSON.resultData kruskal){
+            this.graph_id = id;
+            this.input_stats = Map.of("vertices", vertices, "edges",edges);
+            this.prim = prim;
+            this.kruskal = kruskal;
+        }
+
+        public int getId(){
+            return graph_id;
+        }
+
+        public Map<String,Integer> inputStats(){
+            return input_stats;
+        }
+
+        public readFromJSON.resultData getP(){
+            return kruskal;
+        }
+
+        public readFromJSON.resultData getK(){
+            return kruskal;
+        }
+    }
+
+    public static void outputDataToJSON(List<resultMST> resultGraphs) {
+        Gson gson = new GsonBuilder().setPrettyPrinting().create();
+        try (FileWriter writer = new FileWriter("assign3_output.json")) {
+            gson.toJson(resultGraphs, writer);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
